fedca255a152be30fcb5dcf430be933d150bbb77
Jeff Dairiki <dairiki@dairiki.org>
2022-10-16 13:43:57 -0700

    Increase timeout in ShellModeRunner test
    This hopefully will fix spurious GH CI test failures on macos.

--- tests/inkscape/test_runner.py
+++ tests/inkscape/test_runner.py
@@ -99,7 +99,7 @@ class TestShellModeRunner:
             api=api,
             executable=sys.executable,
             inkscape_args=(dummy_inkscape_py,),
-            timeout=3,
+            timeout=10,
         )
 
     def test_success(self, runner: Runner, caplog: pytest.LogCaptureFixture) -> None:
